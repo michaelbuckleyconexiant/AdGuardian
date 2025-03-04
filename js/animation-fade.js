a2f1f1261947f777f1ef7e0b3bbebe5bbc7ccbcd
michaelbuckleyconexiant <michael.buckley@conexiant.com>
2025-03-04 13:56:18 -0500

    updates

---
+++ js/animation-fade.js
@@ -0,0 +1,17 @@
+//fade in animation 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".fade-in, .fade-in-left, .fade-in-right, .fade-in-bottom");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, { threshold: 0.3 });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
